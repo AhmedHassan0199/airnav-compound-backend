b3b388003a125c9fe374cea657cb052e741fdbf1
Ahmed Hassan <ahmedhassanezz01@gmail.com>
2025-11-14 15:45:43 +0200

    Add backend deploy workflow FIX 4

--- app/__init__.py
+++ app/__init__.py
@@ -18,7 +18,8 @@ def create_app():
         origins=[
             "http://localhost:3000",
             "http://127.0.0.1:3000",
-            "https://airnav-compound-frontend.vercel.app"  # ADD YOUR VERCEL URL
+            "https://airnav-compound-frontend.vercel.app",  # ADD YOUR VERCEL URL,
+            "http://95.179.181.72/:3000"
         ],
         supports_credentials=True,
     )
